23902225e76335245c00b8f667a3d2904a31e770
ali <ali.gout.12@hotmail.com>
2022-06-10 10:00:12 +0200

    lichtschakelaar in js

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -5,6 +5,23 @@ document.body.appendChild(btn);
 
 // schijf hier tussen je code
 
+var button = document.querySelector("button")
+oldText();
+
+function updateText() {
+    button.onclick = oldText;
+    btn.innerHTML = 'Switch light off';
+    document.body.style.backgroundColor = "yellow";
+    console.log("light is on");
+}
+
+function oldText() {
+    button.onclick = updateText;
+    btn.innerHTML = 'Switch light on';
+    document.body.style.backgroundColor = "black";
+    console.log("light is off");
+}   
+
 
 
 // schijf hier tussen je code
